b7d2833be75b6a26b4f2d3f462964b9093b90281
Kalib Hackin <nagui08@hotmail.com>
2019-11-12 18:09:37 -0500

    error message done

--- src/partials/onError.js
+++ src/partials/onError.js
@@ -2,6 +2,7 @@ let error = document.getElementById('error');
 
 let onError = city => {
   error.innerHTML = `${city} doesn't exist. Try another one.`
+  setTimeout(() => error.remove(), 3000);
 }
 
 export { onError };
