d29bdc530b45b51ed36e032829510ac5ec08c7fa
Kalib Hackin <nagui08@hotmail.com>
2019-11-12 23:08:41 -0500

    adding giphy images

--- src/partials/apiCall.js
+++ src/partials/apiCall.js
@@ -2,6 +2,7 @@ const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
 let cityName = 'cityName';
 const APP_ID = '&APPID=63483644a53e7a45025b926c656d5f59';
 const UNIT = '&units=metric';
+const GIPHY_URL = 'http://api.giphy.com/v1/gifs/search?q=image&api_key=g5w7GFA06oaBmRTrSgVCkFRsErNwJ9bw&limit=1';
 
 let findWeather = city => {
   return new Promise((resolve, reject) => {
@@ -14,4 +15,16 @@ let findWeather = city => {
   });
 };
 
-export { findWeather };
+let findImage = image => {
+  return new Promise((resolve, reject) => {
+    let url = `${GIPHY_URL.replace('image', image)}`;
+    $.get(url, data => {
+      resolve(data)
+    })
+    .fail ( () => {
+      reject(image);
+    });
+  });
+}
+
+export { findWeather, findImage };
